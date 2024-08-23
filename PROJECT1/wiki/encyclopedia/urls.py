869713f0ba24613edcb93fac8e33a2902e50261a
vivekdave1992 <130426709+vivekdave1992@users.noreply.github.com>
2024-08-23 18:49:16 +0530

    Entry index and random done

--- PROJECT1/wiki/encyclopedia/urls.py
+++ PROJECT1/wiki/encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("entry/<str:title>",views.entry,name="entry"),
+    path("random/", views.random, name="random"),
 ]
--- PROJECT1/wiki/encyclopedia/views.py
+++ PROJECT1/wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from random import choice
 from . import util
 
 
@@ -8,3 +8,17 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request,title):
+    a = util.get_entry(title)
+    if a:
+        return render(request, "encyclopedia/entry.html", {
+            "entry_title": title,
+            "entry_content":a,
+        })
+    else:
+        return render(request,"encyclopedia/error.html")
+
+def random(request):
+    e_lists = util.list_entries()
+    e_title = choice(e_lists)
+    return entry(request, e_title)
