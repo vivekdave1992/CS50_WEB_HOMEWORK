cf3d83406e9dbe909e20a40c0495a26795285bf5
vivekdave1992 <130426709+vivekdave1992@users.noreply.github.com>
2024-09-07 09:26:04 +0530

    add buyer to listing

--- PROJECT2/commerce/auctions/models.py
+++ PROJECT2/commerce/auctions/models.py
@@ -21,9 +21,11 @@ class Listing(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)  # Automatically adds current date and time
     current_bid = models.DecimalField(max_digits=10, decimal_places=2)  # Proper field for currency
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True,related_name="listing_category")
-    
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True,related_name="buyer")
+
     def __str__(self):
         return f"{self.title} : {self.status}"
+
 
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True,related_name="userComment")
@@ -32,5 +34,8 @@ class Comment(models.Model):
     message = models.CharField(max_length=200)
     objects = models.Manager()
     
+    
     def __str__(self) -> str:
         return f"{self.author} commented on {self.listing}  at {self.comment_time}"
+    
+
