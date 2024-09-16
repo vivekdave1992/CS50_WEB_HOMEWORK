1ff301426a9740850542a1c4ed3627320d003d31
vivekdave1992 <130426709+vivekdave1992@users.noreply.github.com>
2024-09-16 16:58:15 +0530

    Composite added , sent_email added

--- PROJECT3/mail/mail/static/mail/inbox.js
+++ PROJECT3/mail/mail/static/mail/inbox.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  // Submit Composed Email
+  document.querySelector('#compose-form').addEventListener('submit', send_email);
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -20,6 +22,7 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +33,31 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email(event){
+  event.preventDefault();
+  
+  //Save the form value
+  const recipients = document.querySelector('#compose-recipients').value ;
+  const subject =document.querySelector('#compose-subject').value ;
+  const body =document.querySelector('#compose-body').value;
+
+  //Send the email
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+      load_mailbox('sent')
+  });
+  
+
 }
